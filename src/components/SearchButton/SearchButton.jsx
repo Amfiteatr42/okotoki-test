bcfca2f10ce31e3aa50dfabe0c07cbf20e47d061
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2024-06-08 21:23:29 +0300

    add search btn

--- src/components/SearchButton/SearchButton.jsx
+++ src/components/SearchButton/SearchButton.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import { SearchIcon } from "../icons/SearchIcon";
+import { CoinsList } from "../CoinsList/CoinsList";
 import styles from "./SearchButton.module.css";
 
 export const SearchButton = () => {
+  const [isShowDropdown, setIsShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsShowDropdown(!isShowDropdown);
+  };
+
+  const onBackdropClick = (e) => {
+    if (e.target === e.currentTarget) setIsShowDropdown(false);
+  };
+
   return (
-    <button className={styles.searchBtn}>
-      <SearchIcon />
-      <span className={styles.searchText}>Search</span>
-    </button>
+    <>
+      <div className={styles.searchBtnWrapper}>
+        <button
+          className={`${styles.searchBtn} ${isShowDropdown && styles.active}`}
+          type="button"
+          onClick={toggleDropdown}
+        >
+          <SearchIcon />
+          <span className={styles.searchText}>Search</span>
+        </button>
+
+        {isShowDropdown && <CoinsList />}
+      </div>
+      {isShowDropdown && (
+        <div className={styles.backdrop} onClick={onBackdropClick}></div>
+      )}
+    </>
   );
 };
