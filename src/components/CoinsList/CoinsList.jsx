337974c8a2db15a837fec8bbf4e6ffe40c75c6d9
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2024-06-09 00:18:43 +0300

    fetch coins, render coins list

--- src/components/CoinsList/CoinsList.jsx
+++ src/components/CoinsList/CoinsList.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SearchIcon } from "../icons/SearchIcon";
 import { CloseIcon } from "../icons/CloseIcon";
+import { getCoins } from "../../api/getCoins";
+import { EmptyStarIcon } from "../icons/EmptyStarIcon";
 import styles from "./CoinsList.module.css";
 
 export const CoinsList = () => {
   const [query, setQuery] = useState("");
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    getCoins().then(setCoins);
+  }, []);
 
   const onQueryChange = (e) => {
     setQuery(e.target.value);
@@ -17,24 +24,42 @@ export const CoinsList = () => {
   return (
     <div className={styles.dropdown}>
       <div className={styles.inputWrapper}>
-        <SearchIcon width="30px" />
+        <SearchIcon width="24px" height="24px" />
         <input
           className={styles.queryInput}
           type="text"
           name="query"
           value={query}
           onChange={onQueryChange}
+          placeholder="Search..."
           autoFocus
         />
-        <button
-          type="button"
-          onClick={clearInput}
-          className={styles.clearInput}
-          aria-label="clear input"
-        >
-          <CloseIcon color="#354B8C" width="10px" height="10px" />
-        </button>
+        <div className={styles.clearBtnPlaceholder}>
+          {query && (
+            <button
+              type="button"
+              onClick={clearInput}
+              className={styles.clearInput}
+              aria-label="clear input"
+            >
+              <CloseIcon color="#354B8C" width="10px" height="10px" />
+            </button>
+          )}
+        </div>
       </div>
+
+      {coins.length > 0 && (
+        <div className={styles.coinsWrapper}>
+          {coins.map((coin) => (
+            <div className={styles.coin}>
+              <button className={styles.favoritesBtn} type="button">
+                <EmptyStarIcon width="18px" height="18px" />
+              </button>
+              <span className={styles.coinTicker}>{coin}</span>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
